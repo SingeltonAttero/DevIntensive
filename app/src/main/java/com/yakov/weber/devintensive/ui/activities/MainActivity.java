02f5fb208ba55507b0bdfc6300435cf3036e0b28
yakov <cen7ur1on007@gmail.com>
2018-02-12 21:48:54 +0200

    add color, fix style and activity

--- app/src/main/java/com/yakov/weber/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/yakov/weber/devintensive/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.yakov.weber.devintensive;
+package com.yakov.weber.devintensive.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.yakov.weber.devintensive.R;
 
 public class MainActivity extends AppCompatActivity {
 

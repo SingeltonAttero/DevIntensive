8c2c53b6c812bdf78116a2e9c68a1931f6d56ef6
yakov <cen7ur1on007@gmail.com>
2018-02-21 02:29:10 +0200

    add collapsing toolbar, drawerLayout, App, DataManager and PreferenceManager...

--- app/src/main/java/com/yakov/weber/devintensive/utils/ConstantManager.java
+++ app/src/main/java/com/yakov/weber/devintensive/utils/ConstantManager.java
@@ -1,9 +1,16 @@
 package com.yakov.weber.devintensive.utils;
 
-/**
- * Created by yakov on 19.02.18.
- */
+
 
 public class ConstantManager {
     public static final String PREFIX_TAG = "TAG ";
+    public static final String MODE_EDIT_KEY = "MODE_EDIT_KEY";
+
+    public static final String USER_EMAIL_KEY = "USER_EMAIL_KEY";
+    public static final String USER_PHONE_KEY = "USER_PHONE_KEY";
+    public static final String USER_GIT_1_KEY = "USER_GIT_1_KEY";
+    public static final String USER_GIT_2_KEY = "USER_GIT_2_KEY";
+    public static final String USER_GIT_3_KEY = "USER_GIT_3_KEY";
+    public static final String USER_ABOUT_KEY = "USER_ABOUT_KEY";
+    public static final String USER_VK_KEY = "USER_VK_KEY";
 }
